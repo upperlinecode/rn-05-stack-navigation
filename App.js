565408f44fa368f18a5d4261f47094464718cda2
Gabriel Rumbaut <grumbaut@deloitte.com>
2024-02-21 15:38:11 -0500

    feat: add safe area view to app.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProductsScreen from './src/screens/ProductsScreen';
@@ -12,18 +13,26 @@ export default function App() {
   const [cart, setCart] = useState([]);
 
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Group>
-            <Stack.Screen name="Products" component={ProductsScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })} />
-          </Stack.Group>
-          <Stack.Group screenOptions={{ presentation: 'modal' }}>
-            <Stack.Screen name="Confirmation" component={ConfirmationModalScreen} options={{ title: 'Thank you!' }} />
-          </Stack.Group>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </CartContext.Provider>
+    <SafeAreaView style={styles.safeAreaView}>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Group>
+              <Stack.Screen name="Products" component={ProductsScreen} />
+              <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })} />
+            </Stack.Group>
+            <Stack.Group screenOptions={{ presentation: 'modal' }}>
+              <Stack.Screen name="Confirmation" component={ConfirmationModalScreen} options={{ title: 'Thank you!' }} />
+            </Stack.Group>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </CartContext.Provider>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+});
